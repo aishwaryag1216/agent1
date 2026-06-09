0372be165585a2ba1c912fdc938150713a5bdb73
aishwaryag1216 <aishwaryag.1216@gmail.com>
2026-06-09 20:52:04 +0530

    Update lecture_content.py
    model name changed

--- lecture_content.py
+++ lecture_content.py
@@ -41,7 +41,7 @@ def generate_content(subject, topic):
         """
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-2.5-pro",
             contents=prompt
         )
 
